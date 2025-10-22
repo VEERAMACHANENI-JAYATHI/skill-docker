c7fd8c6780df3bac8debf6b9031afb3cd668bd30
Veeramachaneni Jayathi <144053523+VEERAMACHANENI-JAYATHI@users.noreply.github.com>
2025-10-23 00:41:36 +0530

    Update SkillManager.jsx

--- Frontend/frontend/src/components/SkillManager.jsx
+++ Frontend/frontend/src/components/SkillManager.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './style.css';
+import './Style.css';
 
 const SkillManager = () => {
   const [skills, setSkills] = useState([]);
